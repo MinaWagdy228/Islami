60d2bf914761cd4e184359eab5b928b4734d53ec
MinaWagdy228 <minawagdy2228@gmail.com>
2024-12-09 19:55:35 +0200

    Home Screen Navigation Done

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:islami/app_colors.dart';
+import 'package:islami/tabs/hadith/hadith_tab.dart';
+import 'package:islami/tabs/quran/quran_tab.dart';
+import 'package:islami/tabs/radio/radio_tab.dart';
+import 'package:islami/tabs/sebha/sebha_tab.dart';
+import 'package:islami/tabs/time/time_tab.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = 'home screen';
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
   List<String> backgroundImages = [
     'assets/Images/quran_bg.png',
     'assets/Images/hadith_bg.png',
@@ -10,55 +21,58 @@ class HomeScreen extends StatefulWidget {
     'assets/Images/radio_bg.png',
     'assets/Images/time_bg.png',
   ];
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int selectedIndex = 0;
+  List<Widget> tabs = [
+    QuranTab(),
+    HadithTab(),
+    SebhaTab(),
+    RadioTab(),
+    TimeTab()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Image.asset(
-          "assets/Images/quran_bg.png",
+          backgroundImages[selectedIndex],
           height: MediaQuery.sizeOf(context).height,
+          width: MediaQuery.sizeOf(context).width,
         ),
         Scaffold(
-          bottomNavigationBar: Theme(
-            data:
-                Theme.of(context).copyWith(canvasColor: AppColors.primaryDark),
-            //depends on what theme you're using currently,copywith so it only affect this scope
-            child: BottomNavigationBar(
-                currentIndex: selectedIndex,
-                onTap: (index) {
-                  selectedIndex = index;
-                  setState(() {});
-                },
-                items: [
-                  BottomNavigationBarItem(
-                      icon: buildItemInNavigationBar(
-                          index: 0, imageName: "quran"),
-                      label: "Quran"),
-                  BottomNavigationBarItem(
-                      icon: buildItemInNavigationBar(
-                          index: 1, imageName: "hadith"),
-                      label: "Hadith"),
-                  BottomNavigationBarItem(
-                      icon: buildItemInNavigationBar(
-                          index: 2, imageName: "sebha"),
-                      label: "Sebha"),
-                  BottomNavigationBarItem(
-                      icon:
-                          buildItemInNavigationBar(index: 3, imageName: "time"),
-                      label: "Time"),
-                ]),
-          ),
-          body: Column(
-            children: [],
-          ),
-        )
+            bottomNavigationBar: Theme(
+              data: Theme.of(context)
+                  .copyWith(canvasColor: AppColors.primaryDark),
+              //depends on what theme you're using currently,copywith so it only affect this scope
+              child: BottomNavigationBar(
+                  currentIndex: selectedIndex,
+                  onTap: (index) {
+                    selectedIndex = index;
+                    setState(() {});
+                  },
+                  items: [
+                    BottomNavigationBarItem(
+                        icon: buildItemInNavigationBar(
+                            index: 0, imageName: "quran"),
+                        label: "Quran"),
+                    BottomNavigationBarItem(
+                        icon: buildItemInNavigationBar(
+                            index: 1, imageName: "hadith"),
+                        label: "Hadith"),
+                    BottomNavigationBarItem(
+                        icon: buildItemInNavigationBar(
+                            index: 2, imageName: "sebha"),
+                        label: "Sebha"),
+                    BottomNavigationBarItem(
+                        icon: buildItemInNavigationBar(
+                            index: 3, imageName: "radio"),
+                        label: "Radio"),
+                    BottomNavigationBarItem(
+                        icon: buildItemInNavigationBar(
+                            index: 4, imageName: "time"),
+                        label: "Time"),
+                  ]),
+            ),
+            body: tabs[selectedIndex]),
       ],
     );
   }
