e4f441c2238d4611af13d2ea612a8e446a0939d0
MinaWagdy228 <minawagdy2228@gmail.com>
2024-12-09 19:25:18 +0200

    Home Screen Navigation Done

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:islami/app_colors.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = 'home screen';
 
-  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +20,49 @@ class HomeScreen extends StatelessWidget {
           height: MediaQuery.sizeOf(context).height,
         ),
         Scaffold(
-          bottomNavigationBar: BottomNavigationBar(items: []),
+          bottomNavigationBar: Theme(
+            data:
+                Theme.of(context).copyWith(canvasColor: AppColors.primaryDark),
+            //depends on what theme you're using currently,copywith so it only affect this scope
+            child: BottomNavigationBar(
+                currentIndex: selectedIndex,
+                onTap: (index) {
+                  selectedIndex = index;
+                  setState(() {});
+                },
+                items: [
+                  BottomNavigationBarItem(
+                      icon: buildItemInNavigationBar(
+                          index: 0, imageName: "quran"),
+                      label: "Quran"),
+                  BottomNavigationBarItem(
+                      icon: buildItemInNavigationBar(
+                          index: 1, imageName: "hadith"),
+                      label: "Hadith"),
+                  BottomNavigationBarItem(
+                      icon: buildItemInNavigationBar(
+                          index: 2, imageName: "sebha"),
+                      label: "Sebha"),
+                  BottomNavigationBarItem(
+                      icon:
+                          buildItemInNavigationBar(index: 3, imageName: "time"),
+                      label: "Time"),
+                ]),
+          ),
         )
       ],
     );
+  }
+
+  Widget buildItemInNavigationBar(
+      {required int index, required String imageName}) {
+    return selectedIndex == index
+        ? Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(66),
+                color: AppColors.blackBgColor),
+            padding: EdgeInsets.symmetric(vertical: 6, horizontal: 20),
+            child: ImageIcon(AssetImage("assets/Images/icon_$imageName.png")))
+        : ImageIcon(AssetImage("assets/Images/icon_$imageName.png"));
   }
 }
--- lib/my_theme_data.dart
+++ lib/my_theme_data.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'app_colors.dart';
+
 class MyThemeData {
   static final ThemeData darkTheme = ThemeData(
-      scaffoldBackgroundColor: Colors
-          .transparent); // so I can access it without having to create an object of it, just by using its class name.
+      scaffoldBackgroundColor: Colors.transparent,
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        // type: BottomNavigationBarType.fixed,
+        // backgroundColor: AppColors.primaryDark,
+        selectedItemColor: AppColors.whiteColor,
+        unselectedItemColor: AppColors.blackColor,
+      )); // so I can access it without having to create an object of it, just by using its class name.
 // final (semi-immutable) is used as it will be changing during out compiling i.e when writing the code, so const would give an error, but final is computed at runtime which is optimal here, also it you can declare a variable or object using it AND initialize it later in the code, which cosnt won't allow.
 }
