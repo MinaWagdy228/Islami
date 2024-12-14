a4b98fc2f55946b5adf032f650980ab2bf21ac7e
MinaWagdy228 <minawagdy2228@gmail.com>
2024-12-14 14:24:37 +0200

    Quran details are done!

---
+++ lib/tabs/radio/radio_channel.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+import '../../app_colors.dart';
+
+class RadioChannel extends StatefulWidget {
+  final String radioName;
+
+  RadioChannel({super.key, required this.radioName});
+
+  @override
+  State<RadioChannel> createState() => _RadioChannelState();
+}
+
+class _RadioChannelState extends State<RadioChannel> {
+  IconData heartIcon = Icons.favorite;
+
+  IconData volumeIcon = Icons.volume_up;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 140,
+      width: 390,
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: AppColors.primaryDark),
+      child: Stack(alignment: Alignment.bottomCenter, children: [
+        Image.asset("assets/Images/radio_container_bg.png"),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "Radio ${widget.radioName}",
+              style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      if (heartIcon == Icons.favorite_border_outlined) {
+                        heartIcon = Icons.favorite;
+                      } else {
+                        heartIcon = Icons.favorite_border_outlined;
+                      }
+                      setState(() {});
+                    },
+                    icon: Icon(
+                      heartIcon,
+                      size: 30,
+                      color: Colors.black,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.play_arrow,
+                      size: 44,
+                      color: Colors.black,
+                    )),
+                IconButton(
+                    onPressed: () {
+                      if (volumeIcon == Icons.volume_up) {
+                        volumeIcon = Icons.volume_off_rounded;
+                      } else {
+                        volumeIcon = Icons.volume_up;
+                      }
+                      setState(() {});
+                    },
+                    icon: Icon(
+                      volumeIcon,
+                      color: Colors.black,
+                      size: 30,
+                    )),
+              ],
+            )
+          ],
+        ),
+      ]),
+    );
+  }
+}
--- lib/tabs/radio/radio_tab.dart
+++ lib/tabs/radio/radio_tab.dart
@@ -1,9 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:islami/tabs/radio/radio_channel.dart';
+
+import '../../app_colors.dart';
 
 class RadioTab extends StatelessWidget {
   static const String routeName = 'radio_tab';
+
+  List<String> radioNames = [
+    'Ibrahim Al-Akadar',
+    'Al-Qaria Yassen',
+    'Ahmed Al-trabulsi',
+    'Adokali Mohammed Alalim'
+  ];
+
+  IconData heartIcon = Icons.favorite;
+
+  IconData volumeIcon = Icons.volume_up;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: PreferredSize(
+          preferredSize:
+              Size.fromHeight(MediaQuery.sizeOf(context).height * 0.2),
+          child: Image.asset("assets/Images/Logo.png")),
+      body: Column(
+        children: [
+          DefaultTabController(
+              length: 2,
+              child: TabBar(
+                  indicatorColor: Colors.transparent,
+                  unselectedLabelStyle: const TextStyle(color: Colors.white),
+                  labelStyle: const TextStyle(color: Colors.black),
+                  indicator: BoxDecoration(
+                      color: AppColors.primaryDark,
+                      borderRadius: BorderRadius.circular(12)),
+                  tabs: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: 184,
+                      height: 40,
+                      child: const Text(
+                        "Radio",
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      width: 184,
+                      height: 40,
+                      child: const Text(
+                        "Reciters",
+                      ),
+                    ),
+                  ])),
+          Expanded(
+              child: ListView.builder(
+                  itemBuilder: (context, index) =>
+                      RadioChannel(radioName: radioNames[index]),
+                  itemCount: radioNames.length))
+        ],
+      ),
+    );
   }
 }
